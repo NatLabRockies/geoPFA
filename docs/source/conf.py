5822d22f9371bb46585bfd5227380d2bda72c07d
Gui Castelao <guilherme@castelao.net>
2026-02-05 20:29:22 -0700

    cfg: Missing myst_parser in sphinx' extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ release = "0.1.0"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
